512181b90302d3932d47fb18125a23324b77f37b
rudywn <rudy.hrm1@gmail.com>
2022-06-05 20:32:50 +0700

    feat: challenge-05-rudywn

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (name, sorting) => {
+  let sortedName = sorting(name)
+  let result = []
+  for (let i = 0; i < sortedName.length; i++) {
+    result.push(i + 1 + ". " + sortedName[i]);
+  }
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
